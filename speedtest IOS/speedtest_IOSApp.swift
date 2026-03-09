297a965644255a4f31fa397d5ba2eb0ef4cb53d2
Sergeynsk63 <sergey.nsk63@gmail.com>
2026-03-09 16:13:09 +0400

    Initial Commit

---
+++ speedtest IOS/speedtest_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  speedtest_IOSApp.swift
+//  speedtest IOS
+//
+//  Created by Sergey on 09.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct speedtest_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
